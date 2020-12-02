b80075438f3cfd3312f68b3c515fc9410057e3b1
grogowsk <grogowsk@akamai.com>
2020-12-02 11:03:57 +0100

    component styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root', // for angular to locate custom html element and apply the component to
-    template: '<h1>My App</h1>' // will fill the inner of targeted custom element when it processes it
+    selector: 'mw-app', // for angular to locate custom html element and apply the component to
+    // template: `
+    //     <h1>My App</h1>
+    //     <p>My awesome application in Angular</p>
+    //     ` // will fill the inner of targeted custom element when it processes it
+    templateUrl: './app.component.html',
+    // styles: [`
+    //     h1 { color: #ffffff; }
+    //     .description {
+    //         font-style: italic;
+    //         color: green;
+    //     }
+    // `],
+    styleUrls: ['./app.component.css']
 }) // needed at least two arguments: selector and template/template component
 export class AppComponent {
 
