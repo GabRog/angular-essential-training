db68a4e4d996c625a4c395df81d3a28f4d6d7d98
grogowsk <grogowsk@akamai.com>
2020-12-02 11:12:34 +0100

    media item completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component'; // without extension, configured to interpret files as typescript
+import { MediaItemComponent } from './media-item.component';
 
 @NgModule({
     imports: [
         BrowserModule // I am building web-based app, so browser will be needed
     ], // Used to bring in modules that my module will need.
     declarations: [
-        AppComponent
+        AppComponent,
+        MediaItemComponent
     ], // Make components, directives, and pipes (declarables) available to my module that don't come from another module.
     bootstrap: [
         AppComponent // used as a target for bootstrapping the app
